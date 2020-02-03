2acbeecdae7c09046d33c2f0991cdde5c0b47b47
Shadow <taltaro2@gmail.com>
2020-02-03 08:40:14 -0500

    Updated Index

--- src/js/script.js
+++ src/js/script.js
@@ -28,31 +28,31 @@ class Card {
 		} else if (this.value === card2.value) {
 			return this.suit < card2.suit;
 		} 
-		return false;
+			return false;
 	}
 
 	greaterThan(card2) {
     if (this.value > card2.value) {
-      return true;
+      	return true;
     } else if (this.value === card2.value) {
-      return this.suit > card2.suit;
+      	return this.suit > card2.suit;
     }
-    return false;
-  }
+   		return false;
+}
 
-  print() {
-  	return this.values[this.value] + " of " + this.suits[this.suit];
-  }
+  	print() {
+  		return this.values[this.value] + " of " + this.suits[this.suit];
+	}
 }
 
 class Deck {
-  constructor() {
-    this.cards = [];
-    for (let i = 2; i <= 14; i++) {
-      for (let j = 0; j <= 3; j++) {
-        let card = new Card(i, j);
-        this.cards.push(card.print());
-      }
+  	constructor() {
+    	this.cards = [];
+    	for (let i = 2; i <= 14; i++) {
+     	for (let j = 0; j <= 3; j++) {
+        	let card = new Card(i, j);
+      	 	 this.cards.push(card.print());
+    }
     }
     this.shuffle(this.cards);
     this.shuffle(this.cards);
@@ -60,27 +60,27 @@ class Deck {
 
 	removeCard() {
     if (this.cards.length === 0) {
-      return;
+     	 return;
     }
-    return this.cards.pop();
+   		 return this.cards.pop();
   }
 
   shuffle() {
     for (let i = 0; i <= 51; i++) {
       var j = Math.floor(Math.random() * (i + 1));
       const temp = this.cards[i];
-      this.cards[i] = this.cards[j];
-      this.cards[j] = temp;
+      	this.cards[i] = this.cards[j];
+      	this.cards[j] = temp;
     }
-    return this.cards;
+    	return this.cards;
   }
 }
 
 class Player {
   constructor(name) {
-    this.wins = 0;
-    this.card = "";
-    this.name = name;
+    	this.wins = 0;
+    	this.card = "";
+    	this.name = name;
   }
 }
 
@@ -89,9 +89,9 @@ class Game {
     // insert your name here for player 1 and 2
     const name1 = prompt("Player 1 Name");
     const name2 = prompt("Player 2 Name");
-    this.deck = new Deck();
-    this.player1 = new Player(name1);
-    this.player2 = new Player(name2);
+    	this.deck = new Deck();
+    	this.player1 = new Player(name1);
+    	this.player2 = new Player(name2);
   }
   wins(winner) {
     console.log(`${winner} wins this round`);
@@ -106,19 +106,19 @@ class Game {
 playGame() {
 	const cards = this.deck.cards;
 	console.log("Let's Start WAR");
-	while (cards.length >= 2) {
+		while (cards.length >= 2) {
 
 		const player1Card = this.deck.removeCard();
 		const player2Card = this.deck.removeCard();
 		const player1Name = this.player1.name;
 		const player2Name = this.player2.name;
-		this.draw(player1Name, player1Card, player2Name, player2Card);
-			if (player1Card >= player2Card) {
-				this.player1.wins++;
-        		this.wins(this.player1.name);
-      } 		else {
-        		this.player2.wins++;
-        		this.wins(this.player2.name);
+			this.draw(player1Name, player1Card, player2Name, player2Card);
+				if (player1Card >= player2Card) {
+					this.player1.wins++;
+        			this.wins(this.player1.name);
+     			} else {
+        			this.player2.wins++;
+        			this.wins(this.player2.name);
 			}
 		}
 		const win = this.winner(this.player1, this.player2);
@@ -126,11 +126,11 @@ playGame() {
     }
 
 winner(player1, player2) {
-	if (player1.wins === player2.wins) {
-		return "The WAR ended in a tie";
-	} else { 
-		return player1.wins > player2.wins ? player1.name : player2.name;
-	}
+		if (player1.wins === player2.wins) {
+			return "The WAR ended in a tie";
+		} else { 
+			return player1.wins > player2.wins ? player1.name : player2.name;
+		}
 	}
 }
 
